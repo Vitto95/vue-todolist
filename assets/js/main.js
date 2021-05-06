e4b52b2619afc6ae4e93cd25303242296a6894cc
Vitto95 <grimaldi.vitto@gmail.com>
2021-05-06 14:23:07 +0200

    sbarrare e rimuovere oggetto, completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,28 @@
 const app = new Vue({
   el: "#app",
-  data: {},
+  data: {
+    todos: [
+      {
+        text: "Lavare i piatti",
+        checked: false,
+      },
+      {
+        text: "Fare la spesa",
+        checked: false,
+      },
+      {
+        text: "Dare da mangiare a Sally",
+        checked: true,
+      },
+      {
+        text: "Tagliare il prato",
+        checked: false,
+      },
+    ],
+  },
+  methods: {
+    removeObj(index) {
+      this.todos.splice(index, 1);
+    },
+  },
 });
